7a3f4105057522c1b342d987ca196e8523d006e2
AlyGamalAbusenna <alygamal.hai@gmail.com>
2023-12-29 20:59:58 +0200

    php

---
+++ store_page/fetchData.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "car";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Cars";
+$result = $conn->query($sql);
+
+$cardsData = array();
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $carData = array(
+            'brand' => $row['brand'],
+            'model' => $row['model'],
+            'plate_id' => $row['plate_id'],
+            'year' => intval($row['year']),
+            'price_per_day' => intval($row['price_per_day']), // Convert price to integer
+            'body_shape' => $row['body_shape'],
+            'status' => $row['status'],
+            'color' => $row['color'],
+            'car_image' => 'data:image/jpeg;base64,' . base64_encode($row['car_image'])
+        );
+
+        array_push($cardsData, $carData);
+    }
+}
+
+$conn->close();
+
+echo json_encode($cardsData);
+?>
+
--- store_page/script.js
+++ store_page/script.js
@@ -102,7 +102,7 @@ const cardsData_1 = [
   },
 ];
 
-const cardData = [
+const cardData_2 = [
   {
     brand: "Toyota",
     model: "Corolla",
@@ -158,44 +158,59 @@ const cardData = [
     car_image: "https://png.pngtree.com/png-clipart/20210725/original/pngtree-car-white-transport-car-sports-car-png-image_6561580.png",
   },];
   
-function renderCards(cards) {
-  const cardsContainer = document.getElementById("cardsContainer");
-  cardsContainer.innerHTML = "";
+var cardData=[];
 
-  cards.forEach((cardData) => {
-    const card = document.createElement("div");
-    card.className = "card";
+  function fetchData() {
+    // Make an AJAX request to fetchData.php
+    fetch('fetchData.php')
+        .then(response => response.json())
+        .then(fetchedData => {
+          cardData = fetchedData
+          renderCards(fetchedData); // Update the global variable with the fetched data
+        })
+  }
 
-    card.innerHTML = `
-	  <div class="card-img" style="background-image: url(${
-      cardData.car_image
-    });"></div>
-      <div class="card-title">${cardData.brand} ${cardData.model}</div>
-	  <div class="card-subtitle">${cardData.year} | Plate ID: ${cardData.plate_id} | Status: ${cardData.status}</div>
-      <hr class="card-divider">
-      <div class="card-footer">
-          <div class="card-price" style="background-color: ${
-            cardData.status === "reserved" ? "lightyellow" : "lightgreen"
-          };"><span>$</span> <strong>${cardData.price_per_day.toFixed(
-      2
-    )}</strong></div>
-          <button class="card-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path><path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path></svg>
-          </button>
-      </div>
-    `;
 
-    const addButton = card.querySelector(".card-btn");
-    addButton.addEventListener("click", function () {
-      addToCards(cardData.brand + " " + cardData.model, cardData.price_per_day);
+
+
+  function renderCards(cards) {
+    const cardsContainer = document.getElementById("cardsContainer");
+    cardsContainer.innerHTML = "";
+  
+    cards.forEach((cardData) => {
+      const card = document.createElement("div");
+      card.className = "card";
+  
+      card.innerHTML = `
+        <div class="card-img" style="background-image: url(${
+          cardData.car_image
+        });"></div>
+        <div class="card-title">${cardData.brand} ${cardData.model}</div>
+        <div class="card-subtitle">${cardData.year} | Plate ID: ${
+          cardData.plate_id
+        } | Status: ${cardData.status}</div>
+        <hr class="card-divider">
+        <div class="card-footer">
+            <div class="card-price" style="background-color: ${
+              cardData.status === "reserved" ? "lightyellow" : "lightgreen"
+            };"><span>$</span> <strong>${cardData.price_per_day.toFixed(2)}</strong></div>
+            <button class="card-btn">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path><path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path></svg>
+            </button>
+        </div>
+      `;
+  
+      const addButton = card.querySelector(".card-btn");
+      addButton.addEventListener("click", function () {
+        addToCards(cardData.brand + " " + cardData.model, cardData.price_per_day);
+      });
+  
+      cardsContainer.appendChild(card);
     });
+  }
+  
 
-    cardsContainer.appendChild(card);
-  });
-}
-
-
-renderCards(cardData);
+fetchData();
 
 let filteredCards = []; // Store searched cars globally to perform the filter on them directly
 
